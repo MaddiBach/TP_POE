0cb185b68705054644aedad948563f9d62c35911
Mostapha <mostapha-bachir.addi@redstoneformations.fr>
2023-06-01 07:54:06 +0200

    Added Terraform hcl file for ec2 providing

---
+++ TerraForm/steps.sh
@@ -0,0 +1,22 @@
+
+## Configurez votre compte AWS en renseignant vos clés d'accès
+
+export AWS_ACCESS_KEY_ID=<your-access-key>
+export AWS_SECRET_ACCESS_KEY=<your-secret-access-key>
+
+## Initialisation du projet Terraform
+
+terraform init
+
+## Vérification de la syntaxe du code Terraform
+
+terraform validate
+
+## Vérification de la configuration Terraform
+
+terraform plan
+
+## Création de l'infrastructure
+
+terraform apply
+
